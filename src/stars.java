edbab9674ef9b1af75271caf81dce4a4df92ce3e
onur koc <onurkottz@gmail.com>
2022-04-17 17:17:31 +0300

    Diamond

--- src/stars.java
+++ src/stars.java
@@ -7,16 +7,24 @@ public class stars {
         System.out.print("How many line do you want?: ");
         int line = input.nextInt();
 
-        for(int i = 1; i <= line; i ++){
-            for(int space = 1; space <= line - i; space ++){
+        for (int i = 1; i <= line; i++) {
+            for (int space = 1; space <= line - i; space++) {
                 System.out.print(" ");
             }
-            for(int star = 1; star <= (2 * i) - 1; star ++){
+            for (int star = 1; star <= (2 * i) - 1; star++) {
                 System.out.print("*");
             }
+
             System.out.println();
         }
-
-
+        for (int i = 1; i <= line; i++) {
+            for (int space = 1; space <=i ; space ++) {
+                System.out.print(" ");
+            }
+            for (int star = ((2*line)-1); star >= ((2 * i) + 1); star --) {
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
     }
 }
